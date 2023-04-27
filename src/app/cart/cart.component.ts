5338867da8fd3f575e0e103973e07604dd62112c
Elena Arocena <elena.arocena@innoway.es>
2023-04-27 13:32:13 +0200

    Display the cart items

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';;
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-
+  @Input() product!: Product;
+  products = products;
+  items = this.cartService.getItems(); // la propiedad items para almacenar los productos en el carrito. Este código establece los artículos usando el método CartService getItems(). Definiste este método cuando creaste cart.service.ts.
+  constructor(
+    private cartService: CartService
+  ) { }
 }
